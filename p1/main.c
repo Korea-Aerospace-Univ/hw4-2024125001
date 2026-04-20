342f2330bf38dfb0d7fe01dfe8720e98b6801c28
2024125001 <minjungang2484@gmail.com>
2026-04-20 16:10:44 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
 int main(void){
-    int a; 
-    int b;  
-    int c = 0;  
+    int a; // 맞춰야 할 정답
+    int b;  // 추측한 입력값
+    int c = 0; // 시행 횟수
     
-    scanf("%d", &a);
+    scanf("%d", &a); // 정답 입력 받음
     
-    do{
-        c+=1;
-        scanf("%d", &b);
+    do{     // 정답 맞출 때까지 반복
+        
+        scanf("%d", &b); //추측값 입력
+        c+=1; // 시도 횟수 1 늘어남
         if(b>a){
-            printf("%d>?\n", b);
+            printf("%d>?\n", b); // 입력한 값이 정답보다 큰 경우
                 }
         else if(b<a){
-            printf("%d<?\n", b);
+            printf("%d<?\n", b); // 입력한 값이 정답보다 작은 경우
         }
         else{
-            printf("%d==?\n", b);
+            printf("%d==?\n", b); // 입력한 값이 정답과 일치한 경우
             } 
-    }while(a != b);
+    }while(a != b); // 입력값이 정답과 다르면 반복
 
-    printf("%d\n", c);
+    printf("%d\n", c); // 총 시도 횟수 출력
     return 0;
 }
