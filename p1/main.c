a3a43019de65640cc0ae0aec9a8a196d905322bc
2024125001 <minjungang2484@gmail.com>
2026-04-20 15:23:12 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int a; 
+    int b;  
+    int c = 0;  
+    
+    scanf("%d", &a);
+    
+    do{
+        c+=1;
+        scanf("%d", &b);
+        if(b>a){
+            printf("%d>?", b);
+                }
+        else if(b<a){
+            printf("%d<?", b);
+        }
+        else{
+            printf("%d==?", b);
+            } 
+    }while(a != b);
+
+    printf("\n%d", c);
+    return 0;
+}
